d3b745e65a2657c3bdcb723f8d86ef8c64c8257c
Jozef Lumaj <jozef.lumaj@gmail.com>
2022-03-19 10:26:34 -0400

    done

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # infant_deaths_by_nation_wealth
+
+This is a quick (largely visual; though some regression used) analysis of the relative deaths under five for countries which I designate 3 categories for:
+(1) rich (2) middle-class (3) poor, which aim to see the trends of how deaths under 5 has changed.
+
+The ultimate conclusion recieved from this analysis was that middle class and poor countries have had a steeper delcine than rich countries (largely following the same trajectory) in their deaths under five, though this decline only began to occur in the 1990's (this is liklely affected by low infrastructure to record all deaths in the middle class and poor countries; e.g. middle class having higher relative deaths to start off with because higher development to count more deaths). It is very interesting to see that middle class and poor countries have had a very similar trajectory in relative deaths under 5 even though there are worlds of differences between the gdp per capita in those two groups of nations
+
+![Relative Deaths Under 5 Since 1960](./relative_deaths_under_5_by_nation_wealth.png "Relative Deaths Under 5 Since 1960")
+
+![Curve-Linear Model Relative Deaths Under 5 Since 1960](./curvelinearmodel_relative_deaths_under_5_by_nation_wealth.png "Curve-Linear Model Relative Deaths Under 5 Since 1960")
+
+#### Data
+```python
+#from https://data.worldbank.org/indicator/SH.DTH.MORT
+#this data has a list of deaths under the age of 5 for all countries by year starting in 1960 (although some have no or sparse data)
+deaths = pd.read_csv("./deaths_under_5_global.csv")
+
+#from https://www.worldometers.info/gdp/gdp-by-country/
+#this data ranks countries by their GDP
+rankings = pd.read_excel("./gdp_rankings.xlsx")
+```
--- comparing_deaths_under_5.py
+++ comparing_deaths_under_5.py
@@ -2,15 +2,18 @@ import pandas as pd
 import seaborn as sb
 import numpy as np
 import matplotlib.pyplot as plt
-import scipy as sp
+import statsmodels.api as sm
 
+#from https://www.worldometers.info/gdp/gdp-by-country/
+#this data ranks countries by their GDP
 gdp = pd.read_excel("./gdp_rankings.xlsx")
 gdp = gdp[["Country","GDP \nper capita "]]
 gdp["GDP \nper capita "] = gdp["GDP \nper capita "].str.replace("$", "").str.replace(",", "")
 gdp.sort_values('GDP \nper capita ')
 print(gdp)
 
-
+#from https://data.worldbank.org/indicator/SH.DTH.MORT
+#this data has a list of deaths under the age of 5 for all countries by year starting in 1960 (although some have no or sparse data)
 deaths = pd.read_csv("./deaths_under_5_global.csv", header=4)
 
 #make dataframe formatted for induvidual country time series data visualization & later data analysis
@@ -54,32 +57,50 @@ deaths_middle = deaths_4[middle].dropna(axis=1, how="any")
 deaths_poor = deaths_4[poor].dropna(axis=1, how="any")
 
 #add up the three groups' yearly totals
-deaths_rich['total'] = deaths_rich.iloc[:, :].sum(axis=1)
-deaths_middle['total'] = deaths_middle.iloc[:, :].sum(axis=1)
-deaths_poor['total'] = deaths_poor.iloc[:, :].sum(axis=1)
+#divide yearly totals by how many countries sampled in their group
+deaths_rich['total'] = deaths_rich.iloc[:, :].sum(axis=1)/len(deaths_rich)
+deaths_middle['total'] = deaths_middle.iloc[:, :].sum(axis=1)/len(deaths_middle)
+deaths_poor['total'] = deaths_poor.iloc[:, :].sum(axis=1)/len(deaths_poor)
 
-#normalize data min-max
-deaths_rich['total'] = (deaths_rich['total']-deaths_rich['total'].min())/(deaths_rich['total'].max()-deaths_rich['total'].min())
-deaths_middle['total'] = (deaths_middle['total']-deaths_middle['total'].min())/(deaths_middle['total'].max()-deaths_middle['total'].min())
-deaths_poor['total'] = (deaths_poor['total']-deaths_poor['total'].min())/(deaths_poor['total'].max()-deaths_poor['total'].min())
+#normalize totals
+deaths_rich['total'] = (deaths_rich['total']-deaths_rich['total'].mean())/deaths_rich['total'].std()
+deaths_middle['total'] = (deaths_middle['total']-deaths_middle['total'].mean())/deaths_middle['total'].std()
+deaths_poor['total'] = (deaths_poor['total']-deaths_poor['total'].mean())/deaths_poor['total'].std()
 print(deaths_rich)
 
-#plot the three graphs
+#prepare for plotting on a line graph, then plot the three graphs
 total_df = pd.DataFrame().assign(rich=deaths_rich['total'], middle=deaths_middle['total'], poor=deaths_poor['total'], year=deaths_4['Year'])
 total_df.reset_index(inplace=True)
 total_df.drop(columns=['index'], inplace=True)
 print(total_df)
 
-# HAVE TO FIX THE NORMALIZTION; do ti earlier
 graph_df = pd.melt(frame=total_df, id_vars=['year'], value_vars=['middle', 'rich', 'poor'], value_name="relative_deaths")
 sb.lineplot(x="year", y="relative_deaths", hue="variable", data=graph_df)
-plt.show()
+#plt.show()
+graph_df["year"] = pd.to_numeric(graph_df["year"])
+#linear models
+sb.lmplot(x="year", y="relative_deaths", hue="variable", order=2, data=graph_df) 
 
 #do ANOVA on the three groups
+#samples clearly not normal, left skewed; 60 samples each; not independant because dependant on previous years, therefore time-series analysis needed;
+#for simplicity's sake, let us compare the three linear models; they will inherently be significantly different since we have one data point for each year and because I saw the confidence intervals in the linear model plot which do not overlap therefore just take a quick peak at the model stats and call it a day
 
-'''
-RESULTS:
+sb.displot(graph_df, x="relative_deaths", hue="variable")
+#plt.show()
 
-CONCLUSION: 
+rel_rich = total_df['rich'].to_numpy()
+rel_middle = total_df['middle'].to_numpy()
+rel_poor = total_df['poor'].to_numpy()
+year_list = pd.to_numeric(total_df["year"]).to_numpy()
 
-'''
+poisson_model_rich = sm.GLM(year_list, rel_rich, family=sm.families.Poisson())
+results_rich = poisson_model_rich.fit()
+print(results_rich.summary())
+
+poisson_model_middle = sm.GLM(year_list, rel_middle, family=sm.families.Poisson())
+results_middle = poisson_model_middle.fit()
+print(results_middle.summary())
+
+poisson_model_poor = sm.GLM(year_list, rel_poor, family=sm.families.Poisson())
+results_poor = poisson_model_poor.fit()
+print(results_poor.summary())
